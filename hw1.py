50eb27b047e7f569fb4ef18662351ac0a1516568
OlyaVT <olyaprokopovych@gmail.com>
2020-02-07 11:38:44 +0200

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,17 @@
+#простий калькулятор
+
+a=10
+b=5
+print (a+b)
+print (a-b)
+print (a*b)
+print (a/b)
+
+#відповіді на питання
+
+user_name = input("Your name:")
+print ("Your name is", user_name)
+user_age = int(input("Your age:"))
+print ("Your age is", user_age)
+user_address = input ("Where do you live:")
+print ("You live in",  user_address)
